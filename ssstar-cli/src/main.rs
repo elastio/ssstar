1d3ce184ea7eeb2f1be27e84541dfeb6cb07361a
Adam Nelson <anelson@users.noreply.github.com>
2022-09-02 15:50:37 +0200

    Symlink `README.md` from parent so it can be used in Rust package

--- ssstar-cli/src/main.rs
+++ ssstar-cli/src/main.rs
@@ -1,4 +1,4 @@
-#![doc = include_str!("../../README.md")]
+#![doc = include_str!("../README.md")]
 
 use clap::{ArgGroup, Parser, Subcommand};
 use ssstar::{CreateArchiveJobBuilder, ExtractArchiveJobBuilder, SourceArchive, TargetArchive};
